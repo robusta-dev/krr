f6986277347b6aaaffe63d52692746b6500f7d5c
Павел Жуков <33721692+LeaveMyYard@users.noreply.github.com>
2023-04-06 14:51:40 +0300

    Add additional class to robusta_krr.api.models

--- robusta_krr/api/models.py
+++ robusta_krr/api/models.py
@@ -1,11 +1,12 @@
 from robusta_krr.core.abstract.strategies import HistoryData, ResourceRecommendation, RunResult
-from robusta_krr.core.models.allocations import ResourceAllocations, ResourceType
+from robusta_krr.core.models.allocations import RecommendationValue, ResourceAllocations, ResourceType
 from robusta_krr.core.models.objects import K8sObjectData
 from robusta_krr.core.models.result import ResourceScan, Result, Severity
 
 __all__ = [
     "ResourceType",
     "ResourceAllocations",
+    "RecommendationValue",
     "K8sObjectData",
     "Result",
     "Severity",
