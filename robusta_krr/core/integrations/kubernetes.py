0f0506e9988c13edb4d96ee0dae4f38b9ab75694
avi robusta <avi@robusta.dev>
2023-05-16 13:23:10 +0300

    rebase fix

--- robusta_krr/core/integrations/kubernetes.py
+++ robusta_krr/core/integrations/kubernetes.py
@@ -56,9 +56,9 @@ class ClusterLoader(Configurable):
         objects = itertools.chain(*objects_tuple)
         if self.config.namespaces == "*":
             # NOTE: We are not scanning kube-system namespace by default
-            result = [obj for obj in itertools.chain(*objects_tuple) if obj.namespace != "kube-system"]
+            result = [obj for obj in objects if obj.namespace != "kube-system"]
         else:
-            result = [obj for obj in itertools.chain(*objects_tuple) if obj.namespace in self.config.namespaces]
+            result = [obj for obj in objects if obj.namespace in self.config.namespaces]
 
         namespaces = {obj.namespace for obj in result}
         self.info(f"Found {len(result)} objects across {len(namespaces)} namespaces in {self.cluster}")
