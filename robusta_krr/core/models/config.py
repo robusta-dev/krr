f54f88cd39fade523b7d2b1c4a9d8ab76a1dd7d3
avi robusta <avi@robusta.dev>
2023-07-24 14:56:41 +0300

    header fix and increase minimum values

--- robusta_krr/core/models/config.py
+++ robusta_krr/core/models/config.py
@@ -19,8 +19,8 @@ class Config(pd.BaseSettings):
     selector: Optional[str] = None
 
     # Value settings
-    cpu_min_value: int = pd.Field(5, ge=0)  # in millicores
-    memory_min_value: int = pd.Field(10, ge=0)  # in megabytes
+    cpu_min_value: int = pd.Field(100, ge=0)  # in millicores
+    memory_min_value: int = pd.Field(100, ge=0)  # in megabytes
 
     # Prometheus Settings
     prometheus_url: Optional[str] = pd.Field(None)
