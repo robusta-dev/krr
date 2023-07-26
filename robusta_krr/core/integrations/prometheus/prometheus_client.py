dc8a368f1efbd313e20167dfec23b9c987999829
LeaveMyYard <33721692+LeaveMyYard@users.noreply.github.com>
2023-07-26 10:23:20 +0300

    Revert POST for prometheus connector

--- robusta_krr/core/integrations/prometheus/prometheus_client.py
+++ robusta_krr/core/integrations/prometheus/prometheus_client.py
@@ -32,60 +32,60 @@ class CustomPrometheusConnect(PrometheusConnect):
         self._session = requests.Session()
         self._session.mount(self.url, HTTPAdapter(max_retries=retry, pool_maxsize=10, pool_block=True))
 
-    @no_type_check
-    def custom_query(self, query: str, params: dict = None):
-        params = params or {}
-        data = None
-        query = str(query)
-        # using the query API to get raw data
-        response = self._session.post(
-            "{0}/api/v1/query".format(self.url),
-            data={"query": query, **params},
-            verify=self.ssl_verification,
-            headers=self.headers,
-            auth=self.auth,
-        )
-        if response.status_code == 200:
-            data = response.json()["data"]["result"]
-        else:
-            raise PrometheusApiClientException(
-                "HTTP Status Code {} ({!r})".format(response.status_code, response.content)
-            )
+    # @no_type_check
+    # def custom_query(self, query: str, params: dict = None):
+    #     params = params or {}
+    #     data = None
+    #     query = str(query)
+    #     # using the query API to get raw data
+    #     response = self._session.post(
+    #         "{0}/api/v1/query".format(self.url),
+    #         data={"query": query, **params},
+    #         verify=self.ssl_verification,
+    #         headers=self.headers,
+    #         auth=self.auth,
+    #     )
+    #     if response.status_code == 200:
+    #         data = response.json()["data"]["result"]
+    #     else:
+    #         raise PrometheusApiClientException(
+    #             "HTTP Status Code {} ({!r})".format(response.status_code, response.content)
+    #         )
 
-        return data
+    #     return data
 
-    @no_type_check
-    def custom_query_range(
-        self,
-        query: str,
-        start_time: datetime,
-        end_time: datetime,
-        step: str,
-        params: dict = None,
-    ):
-        start = round(start_time.timestamp())
-        end = round(end_time.timestamp())
-        params = params or {}
-        data = None
-        query = str(query)
-        # using the query_range API to get raw data
-        response = self._session.post(
-            "{0}/api/v1/query_range".format(self.url),
-            data={
-                "query": query,
-                "start": start,
-                "end": end,
-                "step": step,
-                **params,
-            },
-            verify=self.ssl_verification,
-            headers=self.headers,
-            auth=self.auth,
-        )
-        if response.status_code == 200:
-            data = response.json()["data"]["result"]
-        else:
-            raise PrometheusApiClientException(
-                "HTTP Status Code {} ({!r})".format(response.status_code, response.content)
-            )
-        return data
+    # @no_type_check
+    # def custom_query_range(
+    #     self,
+    #     query: str,
+    #     start_time: datetime,
+    #     end_time: datetime,
+    #     step: str,
+    #     params: dict = None,
+    # ):
+    #     start = round(start_time.timestamp())
+    #     end = round(end_time.timestamp())
+    #     params = params or {}
+    #     data = None
+    #     query = str(query)
+    #     # using the query_range API to get raw data
+    #     response = self._session.post(
+    #         "{0}/api/v1/query_range".format(self.url),
+    #         data={
+    #             "query": query,
+    #             "start": start,
+    #             "end": end,
+    #             "step": step,
+    #             **params,
+    #         },
+    #         verify=self.ssl_verification,
+    #         headers=self.headers,
+    #         auth=self.auth,
+    #     )
+    #     if response.status_code == 200:
+    #         data = response.json()["data"]["result"]
+    #     else:
+    #         raise PrometheusApiClientException(
+    #             "HTTP Status Code {} ({!r})".format(response.status_code, response.content)
+    #         )
+    #     return data
