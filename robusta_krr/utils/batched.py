58c0d0d9401507c3f6d9f9167d2d10d4cb8b4230
LeaveMyYard <zhukovpavel2001@gmail.com>
2023-08-23 18:53:33 +0300

    Rework current pods query to filter finished ones

---
+++ robusta_krr/utils/batched.py
@@ -0,0 +1,15 @@
+import itertools
+from typing import Iterable, TypeVar
+
+
+_T = TypeVar("_T")
+
+
+def batched(iterable: Iterable[_T], n: int) -> Iterable[list[_T]]:
+    "Batch data into tuples of length n. The last batch may be shorter."
+    # batched('ABCDEFG', 3) --> ABC DEF G
+    if n < 1:
+        raise ValueError("n must be at least one")
+    it = iter(iterable)
+    while batch := list(itertools.islice(it, n)):
+        yield batch
