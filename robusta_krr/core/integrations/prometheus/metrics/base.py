55e2809361396d828a4597c903ae0abff57cc945
Ganesh Rathinavel Medayil <ganeshrvel@outlook.com>
2024-02-27 18:18:48 +0530

    Upgraded prometrix.

--- robusta_krr/core/integrations/prometheus/metrics/base.py
+++ robusta_krr/core/integrations/prometheus/metrics/base.py
@@ -117,16 +117,17 @@ class PrometheusMetric(BaseMetric):
 
     def _query_prometheus_sync(self, data: PrometheusMetricData) -> list[PrometheusSeries]:
         if data.type == QueryType.QueryRange:
-            value = self.prometheus.custom_query_range(
+            response = self.prometheus.safe_custom_query_range(
                 query=data.query,
                 start_time=data.start_time,
                 end_time=data.end_time,
                 step=data.step,
             )
-            return value
+            return response["result"]
         else:
             # regular query, lighter on preformance
-            results = self.prometheus.custom_query(query=data.query)
+            response = self.prometheus.safe_custom_query(query=data.query)
+            results = response["result"]
             # format the results to return the same format as custom_query_range
             for result in results:
                 result["values"] = [result.pop("value")]
