0bc47368c546fd0d546a831467bc964c89fd5a90
João Gonçalves <30077840+joaopedrocg27@users.noreply.github.com>
2024-05-13 10:05:29 +0100

    fix(discovery): Added selector for prometheus-stack

--- robusta_krr/core/integrations/prometheus/metrics_service/prometheus_metrics_service.py
+++ robusta_krr/core/integrations/prometheus/metrics_service/prometheus_metrics_service.py
@@ -35,6 +35,7 @@ class PrometheusDiscovery(MetricsServiceDiscovery):
         return super().find_url(
             selectors=[
                 "app=kube-prometheus-stack-prometheus",
+                "app=stack-prometheus",
                 "app=prometheus,component=server",
                 "app=prometheus-server",
                 "app=prometheus-operator-prometheus",
