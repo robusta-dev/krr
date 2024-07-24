50cb7751dfb89f1866c943c0b14bdddb528912fb
Avi-Robusta <97387909+Avi-Robusta@users.noreply.github.com>
2024-07-24 10:50:25 +0300

    add cluster summary of cores and memory (#309)

--- robusta_krr/core/runner.py
+++ robusta_krr/core/runner.py
@@ -286,6 +286,12 @@ class Runner:
 
         with ProgressBar(title="Calculating Recommendation") as self.__progressbar:
             workloads = await self._k8s_loader.list_scannable_objects(clusters)
+            if not clusters or len(clusters) == 1:
+                cluster_name = clusters[0] if clusters else None # its none if krr is running inside cluster
+                prometheus_loader = self._get_prometheus_loader(cluster_name)
+                cluster_summary = await prometheus_loader.get_cluster_summary()
+            else:
+                cluster_summary = {}
             scans = await asyncio.gather(*[self._gather_object_allocations(k8s_object) for k8s_object in workloads])
 
         successful_scans = [scan for scan in scans if scan is not None]
@@ -308,6 +314,7 @@ class Runner:
                 name=str(self._strategy).lower(),
                 settings=self._strategy.settings.dict(),
             ),
+            clusterSummary=cluster_summary
         )
 
     async def run(self) -> int:
