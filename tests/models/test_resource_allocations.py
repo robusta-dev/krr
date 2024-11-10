9a0fe408a595b3c259d2e4a5f3dda601426540a7
Mohse Morad <moshemorad12340@gmail.com>
2024-11-10 13:46:43 +0200

    Handle resources allocation with scientific notation better

---
+++ tests/models/test_resource_allocations.py
@@ -0,0 +1,33 @@
+import pytest
+from robusta_krr.core.models.allocations import ResourceAllocations, ResourceType
+
+
+@pytest.mark.parametrize(
+    "cpu",
+    [
+        {"request": "5m", "limit": None},
+        {"request": 0.005, "limit": None},
+    ],
+)
+@pytest.mark.parametrize(
+    "memory",
+    [
+        {"request": 128974848, "limit": 128974848},
+        {"request": "128.974848e6", "limit": 128.974848e6},
+        {"request": "128.9748480M", "limit": "128.9748480M"},
+        {"request": "128974848000m", "limit": "128974848000m"},
+        {"request": "123Mi", "limit": "123Mi"},
+        {"request": "128974848e0", "limit": "128974848e0"},
+    ],
+)
+def test_resource_allocation_supported_formats(
+    cpu: dict[str, str | None] | dict[str, float | None], memory: dict[str, int] | dict[str, float] | dict[str, str]
+):
+    allocations = ResourceAllocations(
+        requests={ResourceType.CPU: cpu["request"], ResourceType.Memory: memory["request"]},
+        limits={ResourceType.CPU: cpu["limit"], ResourceType.Memory: memory["limit"]},
+    )
+    assert allocations.requests[ResourceType.CPU] == 0.005
+    assert allocations.limits[ResourceType.CPU] == None
+    assert (allocations.requests[ResourceType.Memory] // 1) == 128974848.0
+    assert (allocations.limits[ResourceType.Memory] // 1) == 128974848.0
