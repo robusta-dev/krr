2b63d1e6cfaa3ecd3829fc230af6acd121752091
Saketh kappala <43525626+sappusaketh@users.noreply.github.com>
2025-01-08 20:51:23 +0530

    ci: revert ci changes
    Signed-off-by: Saketh kappala <43525626+sappusaketh@users.noreply.github.com>

--- build_release.sh
+++ build_release.sh
@@ -1,10 +1,5 @@
-#!/bin/bash
-
-python_version=$(cat .python-version)
-
 docker buildx build \
   --build-arg BUILDKIT_INLINE_CACHE=1 \
-  --build-arg "PYTHON_VERSION=${python_version}" \
   --platform linux/arm64,linux/amd64 \
   --tag us-central1-docker.pkg.dev/genuine-flight-317411/devel/krr:${TAG} \
   --push \
