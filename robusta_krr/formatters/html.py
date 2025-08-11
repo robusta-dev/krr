2e1348009c0ba2bafa49fc0a199d01c6d7f55edb
mlhv <levuhoangminh1@gmail.com>
2025-08-11 16:02:49 -0400

    added option to pass console width setting to html export format

--- robusta_krr/formatters/html.py
+++ robusta_krr/formatters/html.py
@@ -2,11 +2,12 @@ from rich.console import Console
 
 from robusta_krr.core.abstract import formatters
 from robusta_krr.core.models.result import Result
+from robusta_krr.core.models.config import settings
 from .table import table
 
 @formatters.register("html")
 def html(result: Result) -> str:
-    html_width = 250
+    html_width = settings.width if settings.width is not None else None
     console = Console(record=True, width=html_width, force_terminal=False)
     table_output = table(result)
     console.print(table_output)
